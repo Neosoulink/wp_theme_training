d88c7463cbe413af863cad5a1c521bce336d625f
Neosoulink <neodarksoulink@gmail.com>
2021-09-27 20:10:50 +0100

    Add front-page.php

---
+++ wp-content/themes/custom/front-page.php
@@ -0,0 +1,10 @@
+<?php get_header(null, []) ?>
+
+<div class="my-5">
+	<h1 class="display-4"><?php the_title() ?></h1>
+	<div class="">
+		<?php the_content() ?>
+	</div>
+</div>
+
+<?php get_footer(null, []) ?>
