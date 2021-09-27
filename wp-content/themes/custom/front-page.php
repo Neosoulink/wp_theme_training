cb5c2a6cc5f8512084d6afadd53a17e53d964795
Neosoulink <neodarksoulink@gmail.com>
2021-09-27 20:15:36 +0100

    Add link to posts

--- wp-content/themes/custom/front-page.php
+++ wp-content/themes/custom/front-page.php
@@ -1,10 +1,13 @@
 <?php get_header(null, []) ?>
 
-<div class="my-5">
-	<h1 class="display-4"><?php the_title() ?></h1>
-	<div class="">
-		<?php the_content() ?>
+<?php while (have_posts()) : the_post(); ?>
+	<div class="my-5">
+		<h1 class="display-4"><?php the_title() ?></h1>
+
+		<p><?php the_content() ?></p>
+
+		<a href="<?= get_post_type_archive_link('post') ?>">See last news</a>
 	</div>
-</div>
+<?php endwhile; ?>
 
 <?php get_footer(null, []) ?>
