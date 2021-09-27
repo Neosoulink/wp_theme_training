2ca34f7ad1996cb607cd717b9b37894429a56192
Neosoulink <neodarksoulink@gmail.com>
2021-09-27 08:53:21 +0100

    UI Minor

--- wp-content/themes/custom/index.php
+++ wp-content/themes/custom/index.php
@@ -1,4 +1,18 @@
 <?php get_header(null, []) ?>
 
-Hi me in the sus
+<?php if (have_posts()) : ?>
+	<ul>
+		<?php while (have_posts()) : the_post();  ?>
+			<li>
+				<a href="<?php the_permalink() ?>">
+					<?php the_title() ?>
+				</a> -
+				<?php the_author() ?>
+			</li>
+		<?php endwhile  ?>
+	</ul>
+<?php else : ?>
+	No articles...
+<?php endif ?>
+
 <?php get_footer(null, []) ?>
