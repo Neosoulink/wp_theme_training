bd6cf15b3063426b6b487bf8fd6e4510af590de4
Neosoulink <neodarksoulink@gmail.com>
2021-09-27 17:02:34 +0100

    Not found page

---
+++ wp-content/themes/custom/404.php
@@ -0,0 +1,9 @@
+<?php get_header(null, []) ?>
+
+<div class="my-5">
+	<h1 class="display-4">Page Introuvable ☹</h1>
+
+	<p>La page demandé est introuvable...</p>
+</div>
+
+<?php get_footer(null, []) ?>
