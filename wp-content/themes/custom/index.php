843c778d7d7679fae82ed488f4653d829d2ac843
Neosoulink <neodarksoulink@gmail.com>
2021-09-27 09:07:13 +0100

    Update acrticles UI

--- wp-content/themes/custom/index.php
+++ wp-content/themes/custom/index.php
@@ -1,16 +1,21 @@
 <?php get_header(null, []) ?>
 
 <?php if (have_posts()) : ?>
-	<ul>
+	<div class="row my-5">
 		<?php while (have_posts()) : the_post();  ?>
-			<li>
-				<a href="<?php the_permalink() ?>">
-					<?php the_title() ?>
-				</a> -
-				<?php the_author() ?>
-			</li>
+			<div class="col-sm-4">
+				<div class="card">
+					<div class="card-body">
+						<?php the_post_thumbnail("medium", ["class" => "card-img-top", "style" => "height:auto"]) ?>
+						<h5 class="card-title"><?php the_title() ?></h5>
+						<!--<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>-->
+						<p class="card-text"><?php the_content("See more") ?></p>
+						<a href="<?php the_permalink() ?>" class="card-link text-capitalize"><?php the_author() ?></a>
+					</div>
+				</div>
+			</div>
 		<?php endwhile  ?>
-	</ul>
+	</div>
 <?php else : ?>
 	No articles...
 <?php endif ?>
