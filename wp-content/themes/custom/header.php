7ea853f49f1bae89cb403dc0437ed6ad969a15d8
Neosoulink <neodarksoulink@gmail.com>
2021-09-28 18:30:00 +0100

    Customize search form

--- wp-content/themes/custom/header.php
+++ wp-content/themes/custom/header.php
@@ -40,12 +40,10 @@
 						<a class="nav-link disabled">Disabled</a>
 					</li>
 				</ul>
-				<form class="d-flex">
-					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success" type="submit">Search</button>
-				</form>
+
+				<?= get_search_form() ?>
 			</div>
 		</div>
 	</nav>
-	<?= get_search_form() ?>
+
 	<div class="container">
