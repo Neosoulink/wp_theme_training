a3ab4f6912d3426d71253ad4ac0bf50116d6afc5
Neosoulink <neodarksoulink@gmail.com>
2021-09-28 19:18:00 +0100

    Apply menu class

--- wp-content/themes/custom/header.php
+++ wp-content/themes/custom/header.php
@@ -16,7 +16,11 @@
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<?= wp_nav_menu(["theme_location" => "header"]) ?>
+				<?= wp_nav_menu([
+					"theme_location" => "header",
+					"container" => false,
+					"menu_class" => "navbar-nav me-auto mb-2 mb-lg-0"
+				]);?>
 				<!--<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<li class="nav-item">
 						<a class="nav-link active" aria-current="page" href="#">Home</a>
